4670a65084a77dfaa119694cee659b631898c756
WOZAIZHELIWANYOXUIX123 <2993340535@qq.com>
2025-10-20 10:18:18 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,26 +1,32 @@
-pip#!/bin/bash
+#!/bin/bash
 set -e
 
-output_dir="./output-dirs/finetune-mt5"
-accelerate launch run_translation.py \
-    --model_name_or_path models/mt5-Chinese_braille_data \
-    --output_dir $output_dir \
-    --dataset_name "Violet-yo/Chinese-Braille-Dataset-Full-Tone" \
-    --do_train \
-    --do_eval \
-    --num_train_epochs 1.0 \
-    --per_device_train_batch_size=32 \
-    --per_device_eval_batch_size=32 \
-    --fp16 False \
-    --predict_with_generate \
-    --overwrite_output_dir True \
-    --eval_strategy "steps" \
-    --logging_dir "$output_dir/running_logs/" \
-    --seed 42 \
-    --eval_steps 1000 \
-    --logging_steps 1 \
-    --max_source_length 285 \
-    --max_target_length 285 \
-    --val_max_target_length 300 \
-    --use_fast_tokenizer False \
-    --preprocessing_num_workers 8
+output_dir="./save_model"
+
+accelerate launch train.py \
+  --model_name_or_path "./down_model" \
+  --output_dir "$output_dir" \
+  --train_file "./Chinese_braille_data/Parallel Corpus/train.json" \
+  --validation_file "./Chinese_braille_data/Parallel Corpus/val.json" \
+  --test_file "./Chinese_braille_data/Parallel Corpus/test.json" \
+  --source_prefix "translate Chinese to Braille: " \
+  --do_train \
+  --num_train_epochs 15 \
+  --per_device_train_batch_size 16 \
+  --per_device_eval_batch_size 16 \
+  --gradient_accumulation_steps 8 \
+  --overwrite_output_dir True \
+  --seed 42 \
+  --logging_steps 100 \
+  --max_source_length 128 \
+  --max_target_length 300 \
+  --val_max_target_length 300 \
+  --use_fast_tokenizer False \
+  --preprocessing_num_workers 4 \
+  --overwrite_cache True \
+  --warmup_ratio 0.01 \
+  --fp16 False \
+  --save_steps 10000 \
+  --save_strategy "steps" \
+  --learning_rate 5e-5 \
+  --lr_scheduler_type "cosine"
